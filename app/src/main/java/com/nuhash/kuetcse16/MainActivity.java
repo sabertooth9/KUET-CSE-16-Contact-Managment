4be1565a107c5aab63df0f39332f4489a8602bdc
sabertooth9 <2013nuhash@gmail.com>
2019-11-26 22:45:38 +0600

    Bug Fix

--- app/src/main/java/com/nuhash/kuetcse16/MainActivity.java
+++ app/src/main/java/com/nuhash/kuetcse16/MainActivity.java
@@ -1,15 +1,10 @@
 package com.nuhash.kuetcse16;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
-import androidx.coordinatorlayout.widget.CoordinatorLayout;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
-import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
-
+import android.Manifest;
 import android.app.ProgressDialog;
 import android.app.SearchManager;
 import android.content.Context;
+import android.content.pm.PackageManager;
 import android.graphics.Color;
 import android.os.AsyncTask;
 import android.os.Bundle;
@@ -20,6 +15,16 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.widget.ImageView;
 import android.widget.SearchView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.coordinatorlayout.widget.CoordinatorLayout;
+import androidx.core.app.ActivityCompat;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
 import com.bumptech.glide.Glide;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
@@ -29,10 +34,23 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity implements SwipeRefreshLayout.OnRefreshListener {
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode) {
+            case 1: {
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                } else {
+                    Toast.makeText(MainActivity.this, "Permission denied to read your External storage", Toast.LENGTH_SHORT).show();
+                    finish();
+                }
+            }
+        }
+    }
+
     String maindata;
     Display mDisplay;
     data_handler dataHandler;
@@ -47,24 +65,28 @@ public class MainActivity extends AppCompatActivity implements SwipeRefreshLayou
     Toolbar toolbar;
     CoordinatorLayout coordinatorLayout;
     SwipeRefreshLayout swipeRefreshLayout;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.READ_EXTERNAL_STORAGE},
+                1);
         init();
         setSupportActionBar(toolbar);
-        Log.e("DEBUG WIDTH",mDisplay.getWidth()+"");
-        Log.e("DEBUG HIGHT",mDisplay.getHeight()+"");
-        Glide.with(MainActivity.this).load(R.drawable.coverpic).override(858 , 480).into(cover);
-        Snackbar.make(coordinatorLayout,"Swipe Down to Update Data",Snackbar.LENGTH_LONG).show();
+        Log.e("DEBUG WIDTH", mDisplay.getWidth() + "");
+        Log.e("DEBUG HIGHT", mDisplay.getHeight() + "");
+        Glide.with(MainActivity.this).load(R.drawable.coverpic).override(858, 480).into(cover);
+        Snackbar.make(coordinatorLayout, "Swipe Down to Update Data", Snackbar.LENGTH_LONG).show();
     }
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         MenuInflater inflater = getMenuInflater();
         inflater.inflate(R.menu.search_menu, menu);
 
-        searchManager=(SearchManager)getSystemService(Context.SEARCH_SERVICE);
-        searchView=(SearchView)menu.findItem(R.id.search).getActionView();
+        searchManager = (SearchManager) getSystemService(Context.SEARCH_SERVICE);
+        searchView = (SearchView) menu.findItem(R.id.search).getActionView();
         searchView.setSearchableInfo(searchManager.getSearchableInfo(getComponentName()));
         searchView.setMaxWidth(Integer.MAX_VALUE);
         searchView.setQueryHint("Search Roll,Name,Blood,Hometown,College..");
@@ -75,6 +97,7 @@ public class MainActivity extends AppCompatActivity implements SwipeRefreshLayou
                 adapter.getFilter().filter(query);
                 return false;
             }
+
             @Override
             public boolean onQueryTextChange(String newText) {
                 adapter.getFilter().filter(newText);
@@ -86,20 +109,21 @@ public class MainActivity extends AppCompatActivity implements SwipeRefreshLayou
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        int id=item.getItemId();
-        if(id==R.id.search)return  true;
+        int id = item.getItemId();
+        if (id == R.id.search) return true;
         return super.onOptionsItemSelected(item);
     }
 
     @Override
     public void onBackPressed() {
-        if(!searchView.isIconified()){
+        if (!searchView.isIconified()) {
             searchView.setIconified(true);
             return;
         }
         super.onBackPressed();
     }
-    void set_view(){
+
+    void set_view() {
         arr.clear();
         process_JSON();
         adapter = new Adapter(arr, MainActivity.this);
@@ -107,18 +131,19 @@ public class MainActivity extends AppCompatActivity implements SwipeRefreshLayou
         recyclerView.setAdapter(adapter);
         recyclerView.setLayoutManager(layoutManager);
     }
+
     void init() {
-        mDisplay=getWindowManager().getDefaultDisplay();
-        cover=findViewById(R.id.backdrop);
-        coordinatorLayout=findViewById(R.id.coordinate_layout);
-        toolbar=findViewById(R.id.toolbar);
-        swipeRefreshLayout=findViewById(R.id.swipeRefresh);
+        mDisplay = getWindowManager().getDefaultDisplay();
+        cover = findViewById(R.id.backdrop);
+        coordinatorLayout = findViewById(R.id.coordinate_layout);
+        toolbar = findViewById(R.id.toolbar);
+        swipeRefreshLayout = findViewById(R.id.swipeRefresh);
         swipeRefreshLayout.setOnRefreshListener(this);
         swipeRefreshLayout.setColorSchemeColors(
                 getResources().getColor(R.color.colorPrimary),
-                getResources().getColor(android.R.color.holo_green_dark),getResources().getColor(android.R.color.holo_green_light),
-                getResources().getColor(android.R.color.holo_orange_dark),getResources().getColor(android.R.color.holo_orange_light),
-                getResources().getColor(android.R.color.holo_blue_dark),getResources().getColor(android.R.color.holo_blue_light),getResources().getColor(android.R.color.holo_blue_bright));
+                getResources().getColor(android.R.color.holo_green_dark), getResources().getColor(android.R.color.holo_green_light),
+                getResources().getColor(android.R.color.holo_orange_dark), getResources().getColor(android.R.color.holo_orange_light),
+                getResources().getColor(android.R.color.holo_blue_dark), getResources().getColor(android.R.color.holo_blue_light), getResources().getColor(android.R.color.holo_blue_bright));
         /*swipeRefreshLayout.post(new Runnable() {
             @Override
             public void run() {
@@ -138,10 +163,11 @@ public class MainActivity extends AppCompatActivity implements SwipeRefreshLayou
 
     @Override
     public void onRefresh() {
-        GETDATA getdata=new GETDATA();
+        GETDATA getdata = new GETDATA();
         getdata.execute();
         set_view();
     }
+
     private class GETDATA extends AsyncTask<Void, Void, Void> {
         ProgressDialog progressDialog;
 
@@ -173,12 +199,13 @@ public class MainActivity extends AppCompatActivity implements SwipeRefreshLayou
             super.onPostExecute(aVoid);
             progressDialog.dismiss();
             swipeRefreshLayout.setRefreshing(false);
-            Snackbar.make(coordinatorLayout,"Done..!",Snackbar.LENGTH_LONG).show();
+            Snackbar.make(coordinatorLayout, "Done..!", Snackbar.LENGTH_LONG).show();
             set_view();
         }
     }
+
     void process_JSON() {
-        maindata=dataHandler.Give_data();
+        maindata = dataHandler.Give_data();
         student st;
         try {
             JSONArray jsonArray = new JSONArray(maindata);
@@ -192,7 +219,7 @@ public class MainActivity extends AppCompatActivity implements SwipeRefreshLayou
                 st.hometown = jsonObject.getString("Hometown");
                 st.name = jsonObject.getString("Name");
                 st.nickname = jsonObject.getString("Nick Name");
-                st.phone = "0" + String.valueOf(jsonObject.getInt("Phone"));
+                st.phone = "0" + jsonObject.getInt("Phone");
                 st.roll = String.valueOf(jsonObject.getInt("Roll"));
 
                 if (st.bd.isEmpty()) st.bd = "None";
@@ -204,16 +231,16 @@ public class MainActivity extends AppCompatActivity implements SwipeRefreshLayou
                 if (st.phone.isEmpty()) st.phone = "None";
                 if (st.roll.isEmpty()) st.roll = "None";
 
-                st.hometown=st.hometown.toUpperCase();
-                st.college=st.college.toUpperCase();
-                st.name=st.name.toUpperCase();
-                st.nickname=st.nickname.toUpperCase();
-                if(st.blood.length()==2)
-                st.blood=st.blood.substring(0,1).toUpperCase()+st.blood.substring(1);
-                else st.blood=st.blood.substring(0,2).toUpperCase()+st.blood.substring(2);
+                st.hometown = st.hometown.toUpperCase();
+                st.college = st.college.toUpperCase();
+                st.name = st.name.toUpperCase();
+                st.nickname = st.nickname.toUpperCase();
+                if (st.blood.length() == 2)
+                    st.blood = st.blood.substring(0, 1).toUpperCase() + st.blood.substring(1);
+                else st.blood = st.blood.substring(0, 2).toUpperCase() + st.blood.substring(2);
                 arr.add(st);
             }
-            Log.e("DEBUG",arr.size()+"");
+            Log.e("DEBUG", arr.size() + "");
         } catch (JSONException e) {
             e.printStackTrace();
             Log.e("ERROR", e.getMessage());
--- app/src/main/java/com/nuhash/kuetcse16/ProfileActivity.java
+++ app/src/main/java/com/nuhash/kuetcse16/ProfileActivity.java
@@ -1,19 +1,19 @@
 package com.nuhash.kuetcse16;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Display;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.bumptech.glide.Glide;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.textfield.TextInputEditText;
 import com.mikhaellopez.circularimageview.CircularImageView;
-
-import java.util.Objects;
 
 public class ProfileActivity extends AppCompatActivity {
     student sx;
@@ -25,14 +25,14 @@ public class ProfileActivity extends AppCompatActivity {
 
     @Override
     protected void onPause() {
-        overridePendingTransition(R.anim.fadein,R.anim.fadeout);
+        overridePendingTransition(R.anim.fadein, R.anim.fadeout);
         super.onPause();
     }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        overridePendingTransition(R.anim.fadein,R.anim.fadeout);
+        overridePendingTransition(R.anim.fadein, R.anim.fadeout);
         setContentView(R.layout.activity_profile);
         init();
         name.setText(sx.getName());
@@ -51,130 +51,21 @@ public class ProfileActivity extends AppCompatActivity {
         });
         set_image();
     }
-    void set_image(){
-        if (sx.getRoll().equals("1607031")) {
-            propic.setVisibility(View.VISIBLE);
-            Glide.with(ProfileActivity.this).load(R.drawable.noman).override(250, 250).into(propic);
-            //propic.setImageDrawable(getDrawable(R.drawable.noman));
-        } else if (sx.getRoll().equals("1607035")) {
-            propic.setVisibility(View.VISIBLE);
 
-            Glide.with(ProfileActivity.this).load(R.drawable.rahi).override(250, 250).into(propic);
-//            propic.setImageDrawable(getDrawable(R.drawable.rahi));
-        } else if (sx.getRoll().equals("1607053")) {
-            propic.setVisibility(View.VISIBLE);
-//            propic.setImageDrawable(getDrawable(R.drawable.nowshin));
-            Glide.with(ProfileActivity.this).load(R.drawable.nowshin).override(250, 250).into(propic);
-        } else if (sx.getRoll().equals("1607007")) {
-            propic.setVisibility(View.VISIBLE);
-            Glide.with(ProfileActivity.this).load(R.drawable.paul).override(250, 250).into(propic);
-            //propic.setImageDrawable(getDrawable(R.drawable.paul));
-        } else if (sx.getRoll().equals("1607048")) {
-            propic.setVisibility(View.VISIBLE);
-            Glide.with(ProfileActivity.this).load(R.drawable.faisal).override(250, 250).into(propic);
-            //propic.setImageDrawable(getDrawable(R.drawable.faisal));
-        } else if (sx.getRoll().equals("1607058")) {
-            propic.setVisibility(View.VISIBLE);
-            Glide.with(ProfileActivity.this).load(R.drawable.sakib).override(250, 250).into(propic);
-            //propic.setImageDrawable(getDrawable(R.drawable.sakib));
-        } else if (sx.getRoll().equals("1607022")) {
-            propic.setVisibility(View.VISIBLE);
-            Glide.with(ProfileActivity.this).load(R.drawable.sas).override(250, 250).into(propic);
-            ////propic.setImageDrawable(getDrawable(R.drawable.sas));
-        } else if (sx.getRoll().equals("1607102")) {
-            propic.setVisibility(View.VISIBLE);
-            Glide.with(ProfileActivity.this).load(R.drawable.yosuf).override(250, 250).into(propic);
-            //propic.setImageDrawable(getDrawable(R.drawable.yosuf));
-        } else if (sx.getRoll().equals("1607093")) {
-            propic.setVisibility(View.VISIBLE);
-            Glide.with(ProfileActivity.this).load(R.drawable.rohan).override(250, 250).into(propic);
-            //propic.setImageDrawable(getDrawable(R.drawable.rohan));
-        } else if (sx.getRoll().equals("1607087")) {
-            propic.setVisibility(View.VISIBLE);
-            Glide.with(ProfileActivity.this).load(R.drawable.osim).override(250, 250).into(propic);
-            //propic.setImageDrawable(getDrawable(R.drawable.osim));
-        } else if (sx.getRoll().equals("1607078")) {
-            propic.setVisibility(View.VISIBLE);
-            Glide.with(ProfileActivity.this).load(R.drawable.anika).override(250, 250).into(propic);
-            //propic.setImageDrawable(getDrawable(R.drawable.anika));
-        } else if (sx.getRoll().equals("1607014")) {
-            propic.setVisibility(View.VISIBLE);
-            Glide.with(ProfileActivity.this).load(R.drawable.sourav).override(250, 250).into(propic);
-            //propic.setImageDrawable(getDrawable(R.drawable.sourav));
-        } else if (sx.getRoll().equals("1607099")) {
-            propic.setVisibility(View.VISIBLE);
-            Glide.with(ProfileActivity.this).load(R.drawable.joy).override(250, 250).into(propic);
-            //propic.setImageDrawable(getDrawable(R.drawable.joy));
-        } else if (sx.getRoll().equals("1607044")) {
-            propic.setVisibility(View.VISIBLE);
-            Glide.with(ProfileActivity.this).load(R.drawable.dipto).override(250, 250).into(propic);
-            //propic.setImageDrawable(getDrawable(R.drawable.dipto));
-        } else if (sx.getRoll().equals("1607079")) {
-            propic.setVisibility(View.VISIBLE);
-            Glide.with(ProfileActivity.this).load(R.drawable.awal).override(250, 250).into(propic);
-            //propic.setImageDrawable(getDrawable(R.drawable.awal));
-        } else if (sx.getRoll().equals("1607040")) {
-            propic.setVisibility(View.VISIBLE);
-            Glide.with(ProfileActivity.this).load(R.drawable.nuhash_bhai).override(250, 250).into(propic);
-            //propic.setImageDrawable(getDrawable(R.drawable.nuhash_bhai));
-        } else if (sx.getRoll().equals("1607045")) {
-            propic.setVisibility(View.VISIBLE);
-            Glide.with(ProfileActivity.this).load(R.drawable.saikan).override(250, 250).into(propic);
-            //propic.setImageDrawable(getDrawable(R.drawable.saikan));
-        } else if (sx.getRoll().equals("1607030")) {
-            propic.setVisibility(View.VISIBLE);
-            Glide.with(ProfileActivity.this).load(R.drawable.sajal).override(250, 250).into(propic);
-            //propic.setImageDrawable(getDrawable(R.drawable.sajal));
-        } else if (sx.getRoll().equals("1607032")) {
-            propic.setVisibility(View.VISIBLE);
-            Glide.with(ProfileActivity.this).load(R.drawable.alif).override(250, 250).into(propic);
-            //propic.setImageDrawable(getDrawable(R.drawable.alif));
-        } else if (sx.getRoll().equals("1607033")) {
-            propic.setVisibility(View.VISIBLE);
-            Glide.with(ProfileActivity.this).load(R.drawable.diptomagi).override(250, 250).into(propic);
-            //propic.setImageDrawable(getDrawable(R.drawable.diptomagi));
-        } else if (sx.getRoll().equals("1607046")) {
-            propic.setVisibility(View.VISIBLE);
-            Glide.with(ProfileActivity.this).load(R.drawable.nuhashmota).override(250, 250).into(propic);
-            //propic.setImageDrawable(getDrawable(R.drawable.nuhashmota));
-        } else if (sx.getRoll().equals("1607070")) {
-            propic.setVisibility(View.VISIBLE);
-            Glide.with(ProfileActivity.this).load(R.drawable.rudra).override(250, 250).into(propic);
-            //propic.setImageDrawable(getDrawable(R.drawable.rudra));
-        } else if (sx.getRoll().equals("1607006")) {
-            propic.setVisibility(View.VISIBLE);
-            Glide.with(ProfileActivity.this).load(R.drawable.sajid).override(250, 250).into(propic);
-            ////propic.setImageDrawable(getDrawable(R.drawable.sajid));
-        } else if (sx.getRoll().equals("1607023")) {
-            propic.setVisibility(View.VISIBLE);
-            Glide.with(ProfileActivity.this).load(R.drawable.shah).override(250, 250).into(propic);
-            // //propic.setImageDrawable(getDrawable(R.drawable.shah));
-        } else if (sx.getRoll().equals("1607084")) {
-            propic.setVisibility(View.VISIBLE);
-            Glide.with(ProfileActivity.this).load(R.drawable.jisan).override(250, 250).into(propic);
-            ////propic.setImageDrawable(getDrawable(R.drawable.jisan));
-        } else if (sx.getRoll().equals("1607049")) {
-            propic.setVisibility(View.VISIBLE);
-            Glide.with(ProfileActivity.this).load(R.drawable.limon).override(250, 250).into(propic);
-//            //propic.setImageDrawable(getDrawable(R.drawable.limon));
-        } else if (sx.getRoll().equals("1607038")) {
-            propic.setVisibility(View.VISIBLE);
-            Glide.with(ProfileActivity.this).load(R.drawable.niloy).override(250, 250).into(propic);
-            ////propic.setImageDrawable(getDrawable(R.drawable.niloy));
-        } else if (sx.getRoll().equals("1607071")) {
-            propic.setVisibility(View.VISIBLE);
-            Glide.with(ProfileActivity.this).load(R.drawable.pias).override(250, 250).into(propic);
-            ////propic.setImageDrawable(getDrawable(R.drawable.pias));
-        } else if (sx.getRoll().equals("1607041")) {
-            propic.setVisibility(View.VISIBLE);
-            Glide.with(ProfileActivity.this).load(R.drawable.proja).override(250, 250).into(propic);
-//            //propic.setImageDrawable(getDrawable(R.drawable.proja));
-        } else if (sx.getRoll().equals("1607034")) {
-            propic.setVisibility(View.VISIBLE);
-            Glide.with(ProfileActivity.this).load(R.drawable.sakha).override(250, 250).into(propic);
-            ////propic.setImageDrawable(getDrawable(R.drawable.sakha));
+    void set_image() {
+        try {
+            String xx = 'a' + String.valueOf(Integer.valueOf(sx.getRoll().substring(4, 7)));
+            Log.e("DEBUG", xx);
+            int id = getResources().getIdentifier(xx, "drawable", getPackageName());
+            if (id != 0x00000000) {
+                Glide.with(ProfileActivity.this).load(id).override(250, 250).into(propic);
+                propic.setVisibility(View.VISIBLE);
+            }
+        } catch (Exception e) {
+            Toast.makeText(getApplicationContext(), e.getMessage(), Toast.LENGTH_SHORT).show();
         }
     }
+
     void init() {
         mDisplay = getWindowManager().getDefaultDisplay();
         sx = Adapter.stt1;
@@ -188,7 +79,7 @@ public class ProfileActivity extends AppCompatActivity {
         name = findViewById(R.id.text_view_profile_name);
         fab = findViewById(R.id.fab);
         propic = findViewById(R.id.image_view_profile_profile);
-        Glide.with(ProfileActivity.this).load(R.drawable.coverpic).override(858 , 480).into(imageView);
+        Glide.with(ProfileActivity.this).load(R.drawable.coverpic).override(858, 480).into(imageView);
         //fab.setVisibility(View.GONE);
     }
 }
